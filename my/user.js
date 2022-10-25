cf80d38a79ad811171f8af83cdce31d58f475d9a
sumguytho <48988983+sumguytho@users.noreply.github.com>
2022-10-25 22:37:24 +0300

    firefox instead of chrome, fix spaces in package list

---
+++ my/user.js
@@ -0,0 +1,14 @@
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+
+user_pref("dom.security.https_only_mode", true);
+user_pref("dom.security.https_only_mode_ever_enabled", true);
+
+user_pref("browser.startup.page", 3);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.newtabpage.enabled", false);
+
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
